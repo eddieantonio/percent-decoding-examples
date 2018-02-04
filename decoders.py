96bc24a6623788e6c309492eb569b7d783bd1d8e
Eddie Antonio Santos <easantos@ualberta.ca>
2018-02-03 22:42:07 -0700

    Implement partial UTF-8 decoder.

--- decoders.py
+++ decoders.py
@@ -25,19 +25,19 @@ def suggested_method(text):
     """
     The suggested algorithm for decoding percent-encoded UTF-8 strings.
     """
-    unencoded = text.encode('UTF-8')
+    undecoded = text.encode('UTF-8')
     output = b''
-    while b'%' in unencoded:
-        i = unencoded.find(b'%')
-        output += unencoded[:i]
-        unencoded = unencoded[i:]
+    while b'%' in undecoded:
+        i = undecoded.index(b'%')
+        output += undecoded[:i]
+        undecoded = undecoded[i:]
 
-        assert chr(unencoded[0]) == '%'
-        byte = unhexlify(unencoded[1:3])
+        assert chr(undecoded[0]) == '%'
+        byte = unhexlify(undecoded[1:3])
         output += byte
-        unencoded = unencoded[3:]
+        undecoded = undecoded[3:]
 
-    output += unencoded
+    output += undecoded
     return output.decode('UTF-8')
 
 
@@ -49,8 +49,8 @@ def re_sub(text):
         hex_digits = match.group()[1:]
         return unhexlify(hex_digits)
 
-    unencoded = text.encode('UTF-8')
-    output = re.sub(b'%[0-9A-Fa-f]{2}', decode_triplet, unencoded)
+    undecoded = text.encode('UTF-8')
+    output = re.sub(b'%[0-9A-Fa-f]{2}', decode_triplet, undecoded)
     return output.decode('UTF-8')
 
 
@@ -59,15 +59,86 @@ def re_sub_compact(text):
     A more compact version of re_sub()
     """
 
-    unencoded = text.encode('UTF-8')
+    undecoded = text.encode('UTF-8')
     output = re.sub(b'%([0-9A-Fa-f]{2})',
                     lambda m: unhexlify(m.group(1)),
-                    unencoded)
+                    undecoded)
     return output.decode('UTF-8')
+
+
+def partial_utf_8_decoder(undecoded):
+    """
+    Decodes by finding the first percent-encoded string and determining how
+    many more percent-encoded bytes follow it in UTF-8 encoding.
+    """
+
+    def decode_triplet(text):
+        """
+        Helper that decodes the triplet at the beginning of the string.
+        Returns one byte from three characters.
+        """
+        assert text.startswith('%')
+        return bytes.fromhex(text[1:3])
+
+    output = ""
+    while '%' in undecoded:
+        i = undecoded.find('%')
+
+        # Everything up to i is not percent-encoded.
+        output += undecoded[:i]
+        undecoded = undecoded[i:]
+
+        # In order to figure out how many bytes are in this character,
+        # we have to judge it from the first byte of the sequence.
+        first_byte = decode_triplet(undecoded)
+
+        # Length of UTF-8 sequence based on first byte.
+        #
+        # Bits      | Range   | Length
+        # --------- | ------- | -------
+        # 0xxx xxxx | %00-%7f | 1 byte   (ASCII character)
+        # 110x xxxx | %C0-%DF | 2 bytes
+        # 1110 xxxx | %E0-%EF | 3 bytes
+        # 1111 0xxx | %F0-%F7 | 4 bytes
+
+        if first_byte <= b'\x7F':
+            # An ASCII byte.
+            output += first_byte.decode('ASCII')
+            # Take this triple out of the sequence
+            undecoded = undecoded[3:]
+        elif b'\xC0' <= first_byte <= b'\xDF':
+            # Two byte sequence.
+            output += (
+                first_byte +
+                decode_triplet(undecoded[3:])
+            ).decode('UTF-8')
+            undecoded = undecoded[6:]
+        elif b'\xE0' <= first_byte <= b'\xEF':
+            # Three byte sequence.
+            output += (
+                first_byte +
+                decode_triplet(undecoded[3:]) +
+                decode_triplet(undecoded[6:])
+            ).decode('UTF-8')
+            undecoded = undecoded[9:]
+        elif b'\xF0' <= first_byte <= b'\xF7':
+            # Four byte sequence.
+            output += (
+                first_byte +
+                decode_triplet(undecoded[3:]) +
+                decode_triplet(undecoded[6:]) +
+                decode_triplet(undecoded[9:])
+            ).decode('UTF-8')
+            undecoded = undecoded[12:]
+        else:
+            raise ValueError(first_byte)
+    output += undecoded
+    return output
 
 
 decoders = [
     suggested_method,
     re_sub,
-    re_sub_compact
+    re_sub_compact,
+    partial_utf_8_decoder
 ]
